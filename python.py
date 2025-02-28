13fec935ad3f69a7a30268d9d19a53dd6d5b435f
Eduarda Gimenes <168208432+GimenesEduarda@users.noreply.github.com>
2025-02-28 20:41:22 -0300

    Add files via upload

--- python.py
+++ python.py
@@ -1,14 +1,11 @@
 import math
 from turtle import *
 
-
 def hearta(k):
     return 15 * math.sin(k)**3
 
-
 def heartb(k):
     return 12 * math.cos(k) - 5 * math.cos(2 * k) - 2 * math.cos(3 * k) - math.cos(4 * k)
-
 
 speed(0)      
 bgcolor("black") 
@@ -17,7 +14,6 @@ goto(0, -200)
 pendown()        
 color("red")     
 
-# Desenhando o coração
 for i in range(0, 628, 1):  
     x = hearta(i * 0.1) * 20  
     y = heartb(i * 0.1) * 20  
